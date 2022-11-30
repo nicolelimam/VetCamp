59cfe96794614c2e30a66cfbdac2aef23b32fd20
Aluno Fatec <aluno@fateclab.net>
2022-11-30 08:24:39 -0300

    adicionei o banco do veterinário

---
+++ VetCamp/src/dao/vetDAO.java
@@ -0,0 +1,84 @@
+
+package dao;
+
+import dto.vet_atendimentoDTO;
+import java.sql.Connection;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.util.ArrayList;
+import javax.swing.JOptionPane;
+import view.vet_consultaFichaVIEW;
+
+
+public class vetDAO {
+    
+    Connection conn;
+    PreparedStatement pstm;
+    ResultSet rs;
+    
+    ArrayList<vet_atendimentoDTO> listahc = new ArrayList<>();
+    
+    public void cadFicha(vet_atendimentoDTO objatendimento) {
+
+        String sql = "insert into vet_ficha (nome_ficha, idade_ficha, sexo_ficha, categoria_ficha, descricao_ficha, prescricao_ficha) values (?,?,?,?,?,?)";
+        conn = new conexaoDAO().conectaBD();
+
+        try {
+
+            pstm = conn.prepareStatement(sql);
+            pstm.setString(1, objatendimento.getNome());
+            pstm.setString(2, objatendimento.getIdade());
+            pstm.setString(3, objatendimento.getSexo());
+            pstm.setString(4, objatendimento.getCategoria());
+            pstm.setString(5, objatendimento.getDescricao());
+            pstm.setString(6, objatendimento.getPrescricao());
+
+            pstm.execute();
+            pstm.close();
+
+            JOptionPane.showMessageDialog(null, "ATENDIMENTO FINALIZADO!");
+
+        } catch (Exception erro) {
+            JOptionPane.showMessageDialog(null, "vetDAO - cadFicha: " + erro);
+        }
+
+    }
+   
+    public ArrayList<vet_atendimentoDTO> pesquisarConsulta() {
+
+        vet_consultaFichaVIEW objvetconsulta = new vet_consultaFichaVIEW();
+
+        String sql = "SELECT id_ficha, nome_ficha, idade_ficha, sexo_ficha, categoria_ficha from vet_ficha where nome_ficha like '" + objvetconsulta.print + "%'";
+        conn = new conexaoDAO().conectaBD();
+
+        try {
+
+            pstm = conn.prepareStatement(sql);
+            rs = pstm.executeQuery();
+            
+                while (rs.next()) {
+                    
+                    vet_atendimentoDTO objvet = new vet_atendimentoDTO();
+                    objvet.setId(rs.getInt("id_ficha"));
+                    objvet.setNome(rs.getString("nome_ficha"));
+                    objvet.setIdade(rs.getString("idade_ficha"));
+                    objvet.setSexo(rs.getString("sexo_ficha"));
+                    objvet.setCategoria(rs.getString("categoria_ficha"));
+
+                    listahc.add(objvet);
+                    
+                }
+            
+           
+            
+
+        } catch (SQLException erro) {
+
+            JOptionPane.showMessageDialog(null, "atnDAO - pesquisarConsultar: " + erro);
+        }
+
+        return listahc;
+
+    }
+}
---
+++ VetCamp/src/dto/vet_atendimentoDTO.java
@@ -0,0 +1,112 @@
+
+package dto;
+
+
+public class vet_atendimentoDTO {
+    
+    private String nome;
+    private String idade;
+    private String sexo;
+    private String categoria;
+    private String descricao;
+    private String prescricao;
+    private int id;
+
+    /**
+     * @return the nome
+     */
+    public String getNome() {
+        return nome;
+    }
+
+    /**
+     * @param nome the nome to set
+     */
+    public void setNome(String nome) {
+        this.nome = nome;
+    }
+
+    /**
+     * @return the idade
+     */
+    public String getIdade() {
+        return idade;
+    }
+
+    /**
+     * @param idade the idade to set
+     */
+    public void setIdade(String idade) {
+        this.idade = idade;
+    }
+
+    /**
+     * @return the sexo
+     */
+    public String getSexo() {
+        return sexo;
+    }
+
+    /**
+     * @param sexo the sexo to set
+     */
+    public void setSexo(String sexo) {
+        this.sexo = sexo;
+    }
+
+    /**
+     * @return the categoria
+     */
+    public String getCategoria() {
+        return categoria;
+    }
+
+    /**
+     * @param categoria the categoria to set
+     */
+    public void setCategoria(String categoria) {
+        this.categoria = categoria;
+    }
+
+    /**
+     * @return the descricao
+     */
+    public String getDescricao() {
+        return descricao;
+    }
+
+    /**
+     * @param descricao the descricao to set
+     */
+    public void setDescricao(String descricao) {
+        this.descricao = descricao;
+    }
+
+    /**
+     * @return the prescricao
+     */
+    public String getPrescricao() {
+        return prescricao;
+    }
+
+    /**
+     * @param prescricao the prescricao to set
+     */
+    public void setPrescricao(String prescricao) {
+        this.prescricao = prescricao;
+    }
+
+    /**
+     * @return the id
+     */
+    public int getId() {
+        return id;
+    }
+
+    /**
+     * @param id the id to set
+     */
+    public void setId(int id) {
+        this.id = id;
+    }
+}
