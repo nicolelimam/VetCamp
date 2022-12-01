bdd2d7cf3524daa89367abd74bd4d23c71f0b7a9
Nicole Lima <116369597+nicolelimam@users.noreply.github.com>
2022-12-01 15:47:25 -0300

    Adicionei comentários na conexaoDAO

--- VetCamp/src/dao/conexaoDAO.java
+++ VetCamp/src/dao/conexaoDAO.java
@@ -9,10 +9,12 @@ import javax.swing.JOptionPane;
 
 public class conexaoDAO {
     
+    /*É um metodo public do tipo Connection, que liga o arquivo fonte com o banco de dados*/
     public Connection conectaBD() {
 
         Connection conn = null;
 
+         /*Após a conexão, ele irá entrar no método try, pois irá "tentar" fazer algo, se não conseguir ele entra no catch*/
         try {
 
             String url = "jdbc:mysql://localhost:3306/vetcamp?user=root&password=";
@@ -21,6 +23,7 @@ public class conexaoDAO {
         } catch (SQLException erro) {
 
             JOptionPane.showMessageDialog(null, "conexaoDAO - conectaBD: " + erro.getMessage());
+            /*Se der errado, exibe JOptionPane para mostrar ao usuário que a conexão com o banco de dados deu errado, e mostra qual o erro através da variavél erro*/
         }
         return conn;
     }
